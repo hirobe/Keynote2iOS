a4fcd954cdb440768558236985efb5aa49f7f1c7
Kazuya Hirobe <k.hirobe@gmail.com>
2012-04-04 20:58:35 +0900

    UIImageViewとUILabelを出力するようにした

--- Keynote2iOS.py
+++ Keynote2iOS.py
@@ -14,8 +14,10 @@ import xml.dom.minidom
 sys.setrecursionlimit(100000)
 
 headerHight = 20
-frameX = 100
-frameY = 200
+baseLeft = 100
+baseTop = 200
+
+outputScale = 0.5
 
 styleVar = {}
 outputDraws = []
@@ -31,6 +33,55 @@ class MyClass(object):
         '''
         Constructor
         '''
+        
+def log(string):
+    #print '#%s'%string
+    pass
+    
+def output(string):
+    print string
+    
+def outputAddImage(left,top,width,height,filename,parentName):
+    viewName = 'imageView'
+    if filename[-7:]=='@2x.png':
+        filename = filename[:-7]+'.png'
+    
+    print '    UIImageView *%s = [[UIImageView alloc] init];'%viewName
+    print '    %s.frame = CGRectMake(%.1ff, %.1ff, %.1ff, %.1ff)];'%(viewName,left,top,width,height)
+    print '    %s.image = [UIImage imageNamed:@"%s"];'%(viewName,filename)
+    print '    [%s addSubview:%s];'%(parentName,viewName)
+    print '    [%s release];'%viewName
+    print ''
+
+def outputAddLabel(left,top,width,height,text,parentName,styleNames):
+    viewName = 'label'
+    styles = {}
+    for styleName in styleNames:
+        log(styleName)
+        tempStyle = {}
+        if styleName in styleVar.keys():
+            tempStyle = styleVar[styleName]
+            for key in tempStyle.keys():
+                styles[key] = tempStyle[key]
+    
+    print '    UILabel *%s = [[UILabel alloc] init'%viewName
+    print '    %s.frame = CGRectMake(%.1ff, %.1ff, %.1ff, %.1ff)];'%(viewName,left,top,width,height)
+    print '    %s.text = @"%s";'%(viewName,text)
+    if 'fontName' in styles.keys():
+        print '    //fontName:%s'%styles['fontName']
+    if 'fontSize' in styles.keys():
+        print '    %s.font = [UIFont systemFontOfSize:%.1ff];'%(viewName,float(styles['fontSize'])*outputScale)
+    if 'fontColor' in styles.keys():
+        color = styles['fontColor'];
+        print '    %s.textColor = [UIColor colorWithRed:%.2f green:%.2f blue:%.2f alpha:%.2f];'%(viewName,float(color['R']),float(color['G']),float(color['B']),float(color['A']))
+    #print '    statusLabel.shadowColor = [UIColor darkGrayColor];'
+    #print '    statusLabel.shadowOffset = CGSizeMake(0.0f, -1.0f);'
+    print '    %s.backgroundColor = [UIColor clearColor];'%viewName
+    print '    [%s addSubview:%s];'%(parentName,viewName)
+    print '    [%s release];'%viewName
+    print ''
+
+
 def getText(node):
     ret = []
     for n in node.childNodes:
@@ -62,21 +113,23 @@ def parseGeometory(drawObj):
     position = getElm(geometry,'sf:position')
     size = getElm(geometry,'sf:size')
     naturalSize = getElm(geometry,'sf:naturalSize')
-    print ' position:%s,%s'%(position.getAttribute('sfa:x'),
-                            position.getAttribute('sfa:y'))
-    print ' size:%s,%s'%(size.getAttribute('sfa:w'),
-                         size.getAttribute('sfa:h'))
-    return (position.getAttribute('sfa:x'),
-                            position.getAttribute('sfa:y'))
+    log( ' position:%s,%s'%(position.getAttribute('sfa:x'),
+                            position.getAttribute('sfa:y')))
+    log( ' size:%s,%s'%(size.getAttribute('sfa:w'),
+                         size.getAttribute('sfa:h')))
+    return (float(position.getAttribute('sfa:x'))*outputScale,
+            float(position.getAttribute('sfa:y'))*outputScale,
+            float(size.getAttribute('sfa:w'))*outputScale,
+            float(size.getAttribute('sfa:h'))*outputScale)
 
 def parseStyleRefId(drawObj):
     style = getElm(drawObj,'sf:style')
     graphicStyleRef = getElm(style,'sf:graphic-style-ref')
     if graphicStyleRef:
         graphicStyleRefID = graphicStyleRef.getAttribute('sfa:IDREF')
-        print ' styleRefID:%s'%graphicStyleRefID
+        log( ' styleRefID:%s'%graphicStyleRefID )
         if (graphicStyleRefID in styleVar.keys()):
-            print styleVar[graphicStyleRefID]
+            log( styleVar[graphicStyleRefID] )
     return graphicStyleRefID
             
 
@@ -114,9 +167,9 @@ def parseDrawables(drawables):
     for drawObj in drawables.childNodes:
         if drawObj.tagName == 'sf:media':
             imageMedia = getElm(getElm(drawObj,'sf:content'),'sf:image-media')
-            print imageMedia.tagName
+            log( imageMedia.tagName )
             if imageMedia.tagName=='sf:image-media':
-                parseGeometory(drawObj)
+                left,top,width,height=parseGeometory(drawObj)
                 parseStyleRefId(drawObj)
                 
                 unfiltered = getElm(getElm(imageMedia,'sf:filtered-image'),'sf:unfiltered')
@@ -125,24 +178,31 @@ def parseDrawables(drawables):
                     id = unfiltered.getAttribute('sfa:ID')
                     data = getElm(unfiltered,'sf:data')
                     path = data.getAttribute('sf:path')
-                    print ' filePath:%s'%(path)
+                    log( ' filePath:%s'%(path) )
+                    
+                    if path[-4:]=='.jpg':
+                        baseLeft = left
+                        baseTop = top
+                    else:
+                        outputAddImage(left-baseLeft,top-baseTop,width,height,path,'self')
+                    
                     unfilteredImages[id]=path
                     
                     tracedPath = getElm(imageMedia,'sf:traced-path')
                     if tracedPath:
-                        print ' tracedPath:%s'%(tracedPath.getAttribute('sfa:path'))
+                        log( ' tracedPath:%s'%(tracedPath.getAttribute('sfa:path')) )
                 elif unfilteredRef:
                     id = unfilteredRef.getAttribute('sfa:ID')
                     if id in unfilteredImages.keys():
                         path = unfilteredImages[id]
-                        print ' filePath:%s'%(path)
+                        log( ' filePath:%s'%(path) )
                         
                     pass
                 #mediaType = drawObj.getElementsByTagName('st:content')[0].childNodes #[0].tagName
-                print 
+                #print 
         elif drawObj.tagName == 'sf:shape':
-            print "shape"
-            posX,posY=parseGeometory(drawObj)
+            log( "shape")
+            left,top,width,height=parseGeometory(drawObj)
             styleRefId=parseStyleRefId(drawObj)
             
             path = getElm(drawObj,'sf:path')
@@ -151,40 +211,54 @@ def parseDrawables(drawables):
                 if bezierPath:
                     bezier = getElm(bezierPath,'sf:bezier')
                     id = bezier.getAttribute('sfa:ID')
-                    print ' bezier:%s'%(bezier.getAttribute('sfa:path'))
+                    log ( ' bezier:%s'%(bezier.getAttribute('sfa:path')) )
                     #outputDraws = []
-                    outputDraws = createSrcOfBezier(id,bezier,styleRefId,posX,posY)
+                    outputDraws = createSrcOfBezier(id,bezier,styleRefId,left,top)
                     for line in outputDraws:
-                        print line
+                        log( line )
                 pointPath = getElm(path,'sf:point-path')
                 if pointPath:
-                    print ' point-path:%s'%(pointPath.getAttribute('sf:type'))
+                    log (' point-path:%s'%(pointPath.getAttribute('sf:type')) )
                     point = getElm(pointPath,'sf:point')
                     size = getElm(pointPath,'sf:size')
-                    print '  point:%s,%s'%(point.getAttribute('sfa:x'),
-                                            point.getAttribute('sfa:y'))
-                    print '  size:%s,%s'%(size.getAttribute('sfa:w'),
-                                         size.getAttribute('sfa:h'))
+                    log( '  point:%s,%s'%(point.getAttribute('sfa:x'),
+                                            point.getAttribute('sfa:y')))
+                    log( '  size:%s,%s'%(size.getAttribute('sfa:w'),
+                                         size.getAttribute('sfa:h')))
             text = getElm(drawObj,'sf:text')
             if text:
                 textStorage = getElm(drawObj,'sf:text-storage')
                 textBody = getElm(textStorage,'sf:text-body')
                 pList = getElmList(textBody,'sf:p')
-                print ' pList:%s'%(pList)
+                log( ' pList:%s'%(pList) )
                 # UnicodeDecodeError: 'ascii' codec can't decode byte 0xc2 in position 0: ordinal not in range(128)
+                for p in pList:
+                    text = getText(p)
+                    pStyleName = p.getAttribute('sf:style')
+                    if len(text)>0:
+                        outputAddLabel(left-baseLeft,top-baseTop,width,height,text,'self',[pStyleName])
+                    spanList = getElmList(p,'sf:span')
+                    for span in spanList:
+                        text = getText(span)
+                        if len(text)>0:
+                            spanStyleName = span.getAttribute('sf:style')
+                            outputAddLabel(left-baseLeft,top-baseTop,width,height,text,'self',[pStyleName,spanStyleName])
+                    
+
                 text = '¥n'.join(getText(p) for p in pList)
-                print ' text:%s'%(text)
-            print
+                log( ' text:%s'%(text) )
+                 
+                
+            
         elif drawObj.tagName == 'sf:group':
-            print 'group'
+            log( 'group' )
             parseGeometory(drawObj)
             parseDrawables(drawObj)
-            print
                 
     #print 'hoge'
 def parseColor(color):
     colorVar = {}
-    #if color.hasAttribute('sfa:r'):
+    #if color.hasAttributeAttributeAttribute('sfa:r'):
     colorVar['R'] = color.getAttribute('sfa:r')
     colorVar['G'] = color.getAttribute('sfa:g')
     colorVar['B'] = color.getAttribute('sfa:b')
@@ -192,8 +266,8 @@ def parseColor(color):
     return colorVar
 
 def parseStyles(style):
-    styleId = style.getAttribute('sfa:ID')
     propertyVar = {}
+    styleId = style.getAttribute('sfa:ID')
     propertyMap = getElm(style,'sf:property-map')
     if style.tagName == 'sf:graphic-style':
         for property in propertyMap.childNodes:
@@ -231,7 +305,7 @@ def parseStyles(style):
                     propertyVar['strokeColor'] = parseColor(strokeColor)
                 #strokePattern = getElm(stroke,'sf:pattern')
         styleVar[styleId] = propertyVar
-    elif style.tagName=='sf:paragraphstyle':
+    elif style.tagName=='sf:paragraphstyle' or style.tagName=='sf:characterstyle':
         if len(propertyMap.childNodes)>0:
             fontColor = getElm(propertyMap,'sf:fontColor')
             if fontColor:
@@ -239,51 +313,14 @@ def parseStyles(style):
                 if color:
                     propertyVar['fontColor'] = parseColor(color)
             fontSize = getElm(propertyMap,'sf:fontSize')
-            propertyVar['fontSize'] = getElm(fontSize,'sf:number').getAttribute('sfa:number')
+            if fontSize:
+                propertyVar['fontSize'] = getElm(fontSize,'sf:number').getAttribute('sfa:number')
+            fontName = getElm(propertyMap,'sf:fontName')
+            if fontName:
+                propertyVar['fontName'] = getElm(fontName,'sf:string').getAttribute('sfa:string')
     
     styleVar[styleId] = propertyVar    
 
-
-def parseApxm(dir,pageNum):
-    #parseする。forgraundLayerまでたどり着き、あとはdrawablesをparseDrawablesに任せる
-    
-    #page番号はを0からはじめる
-    pageNum-=1
-    #print sys.getrecursionlimit()
-    print 'parsing'
-    print os.path.join(dir,'index.apxl')
-    dom1 = xml.dom.minidom.parse(os.path.join(dir,'index.apxl'))
-    print dom1.documentElement.tagName        
-    slides = getElmList(getElm(dom1,'key:slide-list'),'key:slide')
-    if (pageNum >= len(slides)):
-        print 'Error:slides do not have page %d'%pageNum
-        dom1.unlink()
-        return 
-    slide = slides[pageNum]
-    
-    stylesheet = getElm(slide,'key:stylesheet')
-    styles = getElm(stylesheet,'sf:styles')
-    anonStyles = getElm(stylesheet,'sf:anon-styles')
-    for anonStyle in anonStyles.childNodes:
-        parseStyles(anonStyle)
-    
-    
-    layers = getElm(getElm(slide,'key:page'),'sf:layers')
-    # key:page->sf:layers->sf;layer[sf:type->sf:string.sfa:string='BGSlideForegroundLayer']
-    # ->sf:drawables->sf:media
-    for layer in layers.childNodes:
-        typeString = getElm(getElm(layer,'sf:type'),'sf:string').getAttribute('sfa:string')
-        #print typeString
-        if typeString=='BGSlideForegroundLayer':
-            drawables = getElm(layer,'sf:drawables')
-            parseDrawables(drawables)
-
-#    n = slide
-#    print getText(n)
-#    print n.getAttribute('sfa:ID')
-#    print n.getAttribute('key:depth')
-    dom1.unlink()
-    print 'end parsing'
 
 def unarchive(zipFilePath):
     ## translate to absolate path
@@ -329,6 +366,50 @@ def unarchive(zipFilePath):
     print 'unarchive end.',None,dir
     return dir
 
+
+
+def parseApxm(dir,pageNum):
+    #parseする。forgraundLayerまでたどり着き、あとはdrawablesをparseDrawablesに任せる
+    
+    #page番号はを0からはじめる
+    #print sys.getrecursionlimit()
+    print '================'
+    print 'parsing page %d'%pageNum
+    print '================'
+    pageNum-=1
+    print os.path.join(dir,'index.apxl')
+    dom1 = xml.dom.minidom.parse(os.path.join(dir,'index.apxl'))
+    print dom1.documentElement.tagName        
+    slides = getElmList(getElm(dom1,'key:slide-list'),'key:slide')
+    if (pageNum >= len(slides)):
+        print 'Error:slides do not have page %d'%pageNum
+        dom1.unlink()
+        return 
+    slide = slides[pageNum]
+    
+    stylesheet = getElm(slide,'key:stylesheet')
+    styles = getElm(stylesheet,'sf:styles')
+    anonStyles = getElm(stylesheet,'sf:anon-styles')
+    for anonStyle in anonStyles.childNodes:
+        parseStyles(anonStyle)
+    
+    
+    layers = getElm(getElm(slide,'key:page'),'sf:layers')
+    # key:page->sf:layers->sf;layer[sf:type->sf:string.sfa:string='BGSlideForegroundLayer']
+    # ->sf:drawables->sf:media
+    for layer in layers.childNodes:
+        typeString = getElm(getElm(layer,'sf:type'),'sf:string').getAttribute('sfa:string')
+        #print typeString
+        if typeString=='BGSlideForegroundLayer':
+            drawables = getElm(layer,'sf:drawables')
+            parseDrawables(drawables)
+
+#    n = slide
+#    print getText(n)
+#    print n.getAttribute('sfa:ID')
+#    print n.getAttribute('key:depth')
+    dom1.unlink()
+    print 'end parsing'
 
 def main():
     if len(sys.argv) <2:
